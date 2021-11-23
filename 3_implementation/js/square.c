1a3d4e6e9e0f5e5783a08aa3bdaa18264fa405da
8901086020 <63248297+8901086020@users.noreply.github.com>
2021-11-23 13:58:05 +0530

    Create square.c

---
+++ 3_implementation/js/square.c
@@ -0,0 +1,11 @@
+1.	#include<stdio.h>
+#include<math.h>
+  
+int square(int b)  
+{  
+    int  y;  
+	  printf (" Enter a number to get the Square: ");  
+	    
+    y = b * b ;    
+    printf (" \n The Square of %d is: %d", b, y);  
+}  
