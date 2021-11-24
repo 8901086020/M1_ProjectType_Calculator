186cd855f5846e728010d0a52b170e2a42f5baa7
8901086020 <63248297+8901086020@users.noreply.github.com>
2021-11-24 12:55:55 +0530

    Update main.c

--- 3_implementation/js/main.c
+++ 3_implementation/js/main.c
@@ -10,94 +10,94 @@ int division();
 int modulus();
 int power();
 int factorial();
-double sqrt();
 int square();
 int trigno();
 
+
 int main() {
-  
-  int choice; 
+
+  int choice;
   double degree;
   int n1,n2, ans=0;
   printf("Select the operation you want perform");
-  printf("1.Additon\n 2.Subtraction\n 3.multplication \n 4.Division \n 5.Modules\n 6.Power\n 7.Factorial \n 8.Squareroot\n ");
+  printf("1.Additon\n 2.Subtraction\n 3.multplication \n 4.Division \n 5.Modules\n 6.Power\n 7.Factorial \n 8.Squareroot\n 9.squrae\n 10.trigno");
   scanf("%d", &choice);
 
  switch (choice) {
-    
+
   case 1:
       printf("Enter two number");
       scanf("%d %d",&n1,&n2);
-      ans = addition(n1,n2); 
+      ans = addition(n1,n2);
       printf("answer=%d",ans);
       break;
- 
+
   case 2:
      printf("Enter two number");
       scanf("%d %d",&n1,&n2);
-      ans=subtraction(n1,n2); 
+      ans=subtraction(n1,n2);
       printf("answer=%d",ans);
       break;
-  
+
   case 3:
       printf("Enter two number");
       scanf("%d %d",&n1,&n2);
-      ans = multiplication(n1,n2); 
+      ans = multiplication(n1,n2);
       printf("answer=%d",ans);
       break;
-    
+
   case 4:
       printf("Enter two number");
       scanf("%d %d",&n1,&n2);
-     ans = division(n1,n2);   
+     ans = division(n1,n2);
      printf("answer=%d",ans);
       break;
-    
-  case 5: 
+
+  case 5:
         printf("Enter two number");
         scanf("%d %d",&n1,&n2);
-       ans= modulus(n1,n2); 
+       ans= modulus(n1,n2);
        printf("answer=%d",ans);
-        break;  
-   
-  case 6: 
+        break;
+
+  case 6:
         printf("\nEnter two numbers to find the power \n");
         printf("number: ");
         scanf("%d",&n1);
         printf("power : ");
-        scanf("%d",&n2);  
+        scanf("%d",&n2);
         ans= power(n1,n2);
         printf("answer=%d",ans);
-        break;  
-    
-  case 7: 
+        break;
+
+  case 7:
         printf("\nEnter a number to find factorial : ");
         scanf("%d",&n1);
-        ans = factorial(n1); 
+        ans = factorial(n1);
         printf("answer=%d",ans);
-        break;  
-     
-  case 8: 
+        break;
+
+  case 8:
         printf("\nEnter a number to find sqrt : ");
-        scanf("%lf",&n1);
-        ans = sqrt(n1); 
-        printf("answer=%lf",ans);
-        break;  
-       
-  case 9: 
+        scanf("%d",&n1);
+        ans = sqrt(n1);
+        printf("answer = %d",ans);
+        break;
+
+  case 9:
         printf("\nEnter a number to find square : ");
         scanf("%d",&n1);
-        ans = square(n1); 
+        ans = square(n1);
         printf("answer=%d",ans);
-        break;  
-     
-   case 10: 
+        break;
+
+   case 10:
         printf("\nEnter your angle value in degree : ");
         scanf("%lf",&degree);
-        ans = trigno(degree); 
+        ans = trigno(degree);
         printf("%lf",ans);
-        break;  
-  
+        break;
+
   default:
       printf("Error! operator is not correct");
   }
