8ff88e5b3a65603e36b99a4abe3cfb9a37fa0a1a
8901086020 <63248297+8901086020@users.noreply.github.com>
2021-11-24 14:19:50 +0530

    Update main.c

--- 3_implementation/js/main.c
+++ 3_implementation/js/main.c
@@ -23,40 +23,42 @@ int main() {
   printf("1.Additon\n 2.Subtraction\n 3.multplication \n 4.Division \n 5.Modules\n 6.Power\n 7.Factorial \n 8.Squareroot\n 9.squrae\n 10.trigno");
   scanf("%d", &choice);
 
- switch (choice) {
+  // switch statement
+ 
+  switch (choice) {
 
   case 1:
       printf("Enter two number");
       scanf("%d %d",&n1,&n2);
-      ans = addition(n1,n2);
+      ans = addition(n1,n2);    //calling addition function
       printf("answer=%d",ans);
       break;
 
   case 2:
      printf("Enter two number");
       scanf("%d %d",&n1,&n2);
-      ans=subtraction(n1,n2);
+      ans=subtraction(n1,n2);    //calling subtraction function
       printf("answer=%d",ans);
       break;
 
   case 3:
       printf("Enter two number");
       scanf("%d %d",&n1,&n2);
-      ans = multiplication(n1,n2);
+      ans = multiplication(n1,n2);  //calling multiply function 
       printf("answer=%d",ans);
       break;
 
   case 4:
       printf("Enter two number");
       scanf("%d %d",&n1,&n2);
-     ans = division(n1,n2);
+     ans = division(n1,n2);    //calling division function
      printf("answer=%d",ans);
       break;
 
   case 5:
         printf("Enter two number");
         scanf("%d %d",&n1,&n2);
-       ans= modulus(n1,n2);
+       ans= modulus(n1,n2);         // calling madules function
        printf("answer=%d",ans);
         break;
 
@@ -66,14 +68,14 @@ int main() {
         scanf("%d",&n1);
         printf("power : ");
         scanf("%d",&n2);
-        ans= power(n1,n2);
+        ans= power(n1,n2);                   // calling power function
         printf("answer=%d",ans);
         break;
 
   case 7:
         printf("\nEnter a number to find factorial : ");
         scanf("%d",&n1);
-        ans = factorial(n1);
+        ans = factorial(n1);             //calling  factorial function
         printf("answer=%d",ans);
         break;
 
@@ -87,17 +89,19 @@ int main() {
   case 9:
         printf("\nEnter a number to find square : ");
         scanf("%d",&n1);
-        ans = square(n1);
+        ans = square(n1);     //calling  square function
         printf("answer=%d",ans);
         break;
 
    case 10:
         printf("\nEnter your angle value in degree : ");
         scanf("%lf",&degree);
-        ans = trigno(degree);
+        ans = trigno(degree);     //calling  trigno function
         printf("%lf",ans);
         break;
 
+       // operator doesn't match any case constant
+      
   default:
       printf("Error! operator is not correct");
   }
